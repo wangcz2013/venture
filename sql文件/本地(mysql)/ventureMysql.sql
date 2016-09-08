e3a8131fbc280f8b3f5417884ce7b8e774422ac0
zhaodanfeng023 <2981545625@qq.com>
2016-09-08 09:48:36 +0800

    新增测试记录

--- sql文件/本地(mysql)/ventureMysql.sql
+++ sql文件/本地(mysql)/ventureMysql.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50129
 File Encoding         : 65001
 
-Date: 2016-09-07 10:13:53
+Date: 2016-09-08 09:21:21
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -51,7 +51,7 @@ CREATE TABLE `policyholderman` (
   `amnt` decimal(50,2) NOT NULL,
   `prem` decimal(50,2) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=gbk;
+) ENGINE=InnoDB AUTO_INCREMENT=43 DEFAULT CHARSET=gbk;
 
 -- ----------------------------
 -- Records of policyholderman
@@ -69,6 +69,35 @@ INSERT INTO policyholderman VALUES ('10', '2014', '张三', '男', '1990-02-01',
 INSERT INTO policyholderman VALUES ('11', '2014', '张三', '男', '1990-02-01', '身份证', '1111', '10000.00', '10000000.00');
 INSERT INTO policyholderman VALUES ('12', '2014', '张三', '男', '1990-02-01', '身份证', '1111', '10000.00', '10000000.00');
 INSERT INTO policyholderman VALUES ('13', '2014', '张三', '男', '1990-02-01', '身份证', '1111', '10000.00', '10000000.00');
+INSERT INTO policyholderman VALUES ('14', '2014', '张三', '男', '1990-02-01', '身份证', '1111', '10000.00', '10000000.00');
+INSERT INTO policyholderman VALUES ('15', '2014', '张三', '男', '1990-02-01', '身份证', '1111', '10000.00', '10000000.00');
+INSERT INTO policyholderman VALUES ('16', '2014', '张三', '男', '1990-02-01', '身份证', '1111', '10000.00', '10000000.00');
+INSERT INTO policyholderman VALUES ('17', '2014', '张三', '男', '1990-02-01', '身份证', '1111', '10000.00', '10000000.00');
+INSERT INTO policyholderman VALUES ('18', '2014', '张三', '男', '1990-02-01', '身份证', '1111', '10000.00', '10000000.00');
+INSERT INTO policyholderman VALUES ('19', '2014', '张三', '男', '1990-02-01', '身份证', '1111', '10000.00', '10000000.00');
+INSERT INTO policyholderman VALUES ('20', '2014', '张三', '男', '1990-02-01', '身份证', '1111', '10000.00', '10000000.00');
+INSERT INTO policyholderman VALUES ('21', '2014', '张三', '男', '1990-02-01', '身份证', '1111', '10000.00', '10000000.00');
+INSERT INTO policyholderman VALUES ('22', '2014', '张三', '男', '1990-02-01', '身份证', '1111', '10000.00', '10000000.00');
+INSERT INTO policyholderman VALUES ('23', '2014', '张三', '男', '1990-02-01', '身份证', '1111', '10000.00', '10000000.00');
+INSERT INTO policyholderman VALUES ('24', '2014', '张三', '男', '1990-02-01', '身份证', '1111', '10000.00', '10000000.00');
+INSERT INTO policyholderman VALUES ('25', '2014', '张三', '男', '1990-02-01', '身份证', '1111', '10000.00', '10000000.00');
+INSERT INTO policyholderman VALUES ('26', '2014', '张三', '男', '1990-02-01', '身份证', '1111', '10000.00', '10000000.00');
+INSERT INTO policyholderman VALUES ('27', '2014', '张三', '男', '1990-02-01', '身份证', '1111', '10000.00', '10000000.00');
+INSERT INTO policyholderman VALUES ('28', '2014', '张三', '男', '1990-02-01', '身份证', '1111', '10000.00', '10000000.00');
+INSERT INTO policyholderman VALUES ('29', '2014', '张三', '男', '1990-02-01', '身份证', '1111', '10000.00', '10000000.00');
+INSERT INTO policyholderman VALUES ('30', '2014', '张三', '男', '1990-02-01', '身份证', '1111', '10000.00', '10000000.00');
+INSERT INTO policyholderman VALUES ('31', '2014', '张三', '男', '1990-02-01', '身份证', '1111', '10000.00', '10000000.00');
+INSERT INTO policyholderman VALUES ('32', '2014', '张三', '男', '1990-02-01', '身份证', '1111', '10000.00', '10000000.00');
+INSERT INTO policyholderman VALUES ('33', '2014', '张三', '男', '1990-02-01', '身份证', '1111', '10000.00', '10000000.00');
+INSERT INTO policyholderman VALUES ('34', '2014', '张三', '男', '1990-02-01', '身份证', '1111', '10000.00', '10000000.00');
+INSERT INTO policyholderman VALUES ('35', '2014', '张三', '男', '1990-02-01', '身份证', '1111', '10000.00', '10000000.00');
+INSERT INTO policyholderman VALUES ('36', '2014', '张三', '男', '1990-02-01', '身份证', '1111', '10000.00', '10000000.00');
+INSERT INTO policyholderman VALUES ('37', '2014', '张三', '男', '1990-02-01', '身份证', '1111', '10000.00', '10000000.00');
+INSERT INTO policyholderman VALUES ('38', '2014', '张三', '男', '1990-02-01', '身份证', '1111', '10000.00', '10000000.00');
+INSERT INTO policyholderman VALUES ('39', '2014', '张三', '男', '1990-02-01', '身份证', '1111', '10000.00', '10000000.00');
+INSERT INTO policyholderman VALUES ('40', '2014', '张三', '男', '1990-02-01', '身份证', '1111', '10000.00', '10000000.00');
+INSERT INTO policyholderman VALUES ('41', '2014', '张三', '男', '1990-02-01', '身份证', '1111', '10000.00', '10000000.00');
+INSERT INTO policyholderman VALUES ('42', '2014', '张三', '男', '1990-02-01', '身份证', '1111', '10000.00', '10000000.00');
 
 -- ----------------------------
 -- Table structure for `riskalgorithm`
@@ -88,7 +117,7 @@ CREATE TABLE `riskalgorithm` (
   `modifydate` date NOT NULL,
   `modifytime` varchar(30) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=gbk;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=gbk;
 
 -- ----------------------------
 -- Records of riskalgorithm
@@ -99,6 +128,24 @@ INSERT INTO riskalgorithm VALUES ('3', '2014', '02', '00', '02', '300', 'select 
 INSERT INTO riskalgorithm VALUES ('4', '2015', '01', '01', '01', '111', 'select 111 from dual', '001', '2016-09-06', '21:00:00', '2016-09-06', '21:00:00');
 INSERT INTO riskalgorithm VALUES ('5', '2015', '01', '02', '01', '222', 'select 222 from dual', '001', '2016-09-06', '21:00:00', '2016-09-06', '21:00:00');
 INSERT INTO riskalgorithm VALUES ('6', '2015', '02', '00', '02', '333', 'select 333 from dual', '001', '2016-09-06', '21:00:00', '2016-09-06', '21:00:00');
+INSERT INTO riskalgorithm VALUES ('7', '2014', '02', '00', '02', '324', 'select 324 from dual', '001', '2016-09-08', '09:00:00', '2016-09-08', '09:00:00');
+INSERT INTO riskalgorithm VALUES ('8', '2014', '01', '01', '02', '666', 'select 666 from dual', '001', '2016-09-08', '09:00:00', '2016-09-08', '09:00:00');
+
+-- ----------------------------
+-- Table structure for `riskreflect`
+-- ----------------------------
+DROP TABLE IF EXISTS `riskreflect`;
+CREATE TABLE `riskreflect` (
+  `reflectno` varchar(10) NOT NULL DEFAULT '' COMMENT 'sql语句编码(A001 投保清单sql A002  算法sql )',
+  `reflectname` varchar(100) NOT NULL COMMENT 'sql名称',
+  `cont` varchar(4000) NOT NULL,
+  PRIMARY KEY (`reflectno`)
+) ENGINE=InnoDB DEFAULT CHARSET=gbk;
+
+-- ----------------------------
+-- Records of riskreflect
+-- ----------------------------
+INSERT INTO riskreflect VALUES ('A001', '查询投保人列表清单', 'select idno,riskcode,risktype,sum(amnt) as amnt,sum(prem) as prem  from policyholderper where idno = \'?CONDITION6?\' and riskcode = \'?CONDITION1?\'  and idno = \'?CONDITION6?\'  group by idno,riskcode,risktype');
 
 -- ----------------------------
 -- Table structure for `user`
